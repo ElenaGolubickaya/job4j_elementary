f43c6653b0a2a2c44657fd53881ee92b2d5d16e4
Lena <elenagolubickaya@gmail.com>
2021-01-14 01:52:53 +0300

    6. Идеальный вес [#441595]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -1,18 +1,19 @@
 package ru.job4j;
 
 public class Converter {
-    public static int rubleToEuro(int value){
-        int rsl=value/70;
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
         return rsl;
     }
-    public static  int rubleToDollars(int value){
-        int rsl=value/60;
+
+    public static int rubleToDollars(int value) {
+        int rsl = value / 60;
         return rsl;
     }
 
     public static void main(String[] args) {
-        int euro=Converter.rubleToEuro(350);
-        int dollars=Converter.rubleToDollars(480);
+        int euro = Converter.rubleToEuro(350);
+        int dollars = Converter.rubleToDollars(480);
         System.out.println("350 rubles are " + euro + " euro.");
         System.out.println("480 rubles are " + dollars + " dollars.");
     }
