61600e51f70916f84f32e2535ee8e0e550c9dee6
Lena <elenagolubickaya@gmail.com>
2021-01-31 01:10:34 +0300

    3. Тесты для определения расстояния между точками. [#441630]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -5,15 +5,17 @@ public class Point {
         double x = x2 - x1;
         double y = y2 - y1;
         double a = Math.pow(x, 2) + Math.pow(y, 2);
-        double d = Math.sqrt(a);
-        return d;
+        return Math.sqrt(a);
+
     }
 
     public static void main(String[] args) {
         double result1 = Point.distance(0, 5, 2, 8);
         double result2 = Point.distance(2, 10, 2, 15);
+        double result3 = Point.distance(1, 4, 2, 7);
         System.out.println("result (0, 5) to (2, 8) " + result1);
         System.out.println("result (2, 10) to (2, 15) " + result2);
+        System.out.println("result (1, 4) to (2, 7) " + result3);
 
     }
 }
---
+++ test/ru/job4j/condition/PointTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void when05to28then7() {
+        int x1 = 0;
+        int x2 = 5;
+        int x3 = 2;
+        int x4 = 8;
+        double expected = 7.810249675906654;
+        double rsl1 = Point.distance(0, 5, 2, 8);
+        Assert.assertEquals(expected, rsl1, 0.01);
+    }
+
+    @Test
+    public void when210to215then15() {
+        int x1 = 2;
+        int x2 = 10;
+        int x3 = 2;
+        int x4 = 15;
+        double expected = 15.264337522473747;
+        double rsl2 = Point.distance(2, 10, 2, 15);
+        Assert.assertEquals(expected, rsl2, 0.01);
+
+    }
+
+    @Test
+    public void when14to27then5() {
+        int x1 = 1;
+        int x2 = 4;
+        int x3 = 2;
+        int x4 = 7;
+        double expected = 5.830951894845301;
+        double rsl3 = Point.distance(1, 4, 2, 7);
+        Assert.assertEquals(expected, rsl3, 0.01);
+    }
+}
